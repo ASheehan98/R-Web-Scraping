bb371a98657bb74b80be8c4f0e185d2debdb427f
Alexander Sheehan <aps2@illinois.edu>
2019-05-30 09:12:54 -0500

    Got app working that returns listing and price

--- urlacceptor/app.R
+++ urlacceptor/app.R
@@ -6,17 +6,17 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Car Data"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         textInput("make","make","Enter Make"),
-         textInput("model","model","Enter Model")
+         textInput("make","Vehicle Make","Enter Make"),
+         textInput("model","Vehicle Model","Enter Model")
       ),
       
       mainPanel(
-         textOutput("distPlot")
+         verbatimTextOutput("distPlot")
       )
    )
 )
@@ -27,7 +27,19 @@ server <- function(input, output) {
      make = input$make
      model = input$model
      finalurl = paste0(firsturl,make,plus,model,secondurl,make,thirdurl,model,fourthurl)
-     print(finalurl)
+     cardat = read_html(finalurl)
+     carpric = cardat %>%
+       html_nodes(".s-item__price")%>%
+       html_text()
+     adjusted = priceadjust(carpric)
+     gettitle = function(x) {
+       x %>%
+         html_nodes(".s-item__title")%>%
+         html_text()
+     }
+     listing = gettitle(cardat)
+     finallisting = data.frame("Listing" = listing, "Price" = adjusted)
+     finallisting
 
    })
 }
